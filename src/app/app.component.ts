8b0e21ae3f76904b038764c0869b46425b6bca18
romannawaz <romannawaz01@gmail.com>
2023-07-26 16:15:29 +0300

    feat(app): template changed to product card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './products/page/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'card';
-}
+export class AppComponent {}
