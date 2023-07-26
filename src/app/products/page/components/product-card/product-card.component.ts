e0809a823038627e6dd8d94a242187d26803b18c
romannawaz <romannawaz01@gmail.com>
2023-07-26 16:25:38 +0300

    feat(product-card): default card example

--- src/app/products/page/components/product-card/product-card.component.ts
+++ src/app/products/page/components/product-card/product-card.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+const Material = [MatCardModule, MatButtonModule];
+
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ...Material],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
